a9dd5e06c45ad54868115d88e6ba199f863651c7
kevn7n <kevn_nguyen@yahoo.com>
2021-06-14 17:54:16 -0600

    Add validations in plot spec test

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -1,7 +1,10 @@
 class Plot < ApplicationRecord
+  validates :size, presence: true
+  validates :direction, presence: true
+  validates :number, presence: true, numericality: true
+
   belongs_to :garden
 
   has_many :plant_plots
   has_many :plants, through: :plant_plots
-
 end
