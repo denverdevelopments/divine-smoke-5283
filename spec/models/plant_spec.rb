6527a5968a6e24e3933cf1b1df71edd1e9c1ed02
kevn7n <kevn_nguyen@yahoo.com>
2021-06-14 15:31:31 -0600

    After submit, add plant_plot and plant spec tests

---
+++ spec/models/plant_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Plant do
+  describe 'relationships' do
+    it { should have_many(:plant_plots) }
+    it { should have_many(:plots).through(:plant_plots) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:days_to_harvest) }
+    it { should validate_numericality_of(:days_to_harvest) }
+  end
+end
