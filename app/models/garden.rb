7eddd77a946491f898cc7eecff49fd01920dc063
kevn7n <kevn_nguyen@yahoo.com>
2021-06-14 09:36:01 -0600

    Create table migrations, seeds, routes

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,18 @@
 class Garden < ApplicationRecord
   has_many :plots
+  # has_many :plants, through: :plots ##
+
+  # def self.unique_harvestable #if in PLOTS
+  #   joins(:plants)
+  # end
+
+  def unique_harvestable
+    self.joins(plots: :plants)
+    self.plants.distinct.where("plants.days_to_harvest < ?", 100)
+  end
+
+  # def most_to_least   #Extension attempt, need to fix count
+  #   self.plants.select("plants.*, count(plot.plants) as most_used").order(most_used: :desc)
+  # end
+
 end
